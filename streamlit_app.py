41f10d59f4acbdeabc7122ba1db650a22024aa3c
greg.mottet <mottetgl@gmail.com>
2021-11-12 00:19:19 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,6 @@ import streamlit as st
 pr = pd.read_csv('pr_2018_anomaly_details.csv')
 states = np.unique(pr.state)
 
-sel_states = st.multiselect('Select', states)
-
-pr = pr[pr.state.isin(sel_states)]
-pr['provider_key'] = pr['npi'] + pr['first_name'] + pr['last_name'] + pr['specialty']
-provider_keys = np.unique(pr.provider_key)
-
-sel_provider_key = st.selectbox('Pick one', provider_keys)
-
-pr = pr[pr.provider_key == sel_provider_key]
+# select the state
+sel_states = st.multiselect('Select States', states)
+st.write(sel_states)
