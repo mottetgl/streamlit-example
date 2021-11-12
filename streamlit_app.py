07b9c6bf9a90f5adb453e6bf2efa16d40f29e8d2
mottetgl <31945764+mottetgl@users.noreply.github.com>
2021-11-12 22:15:07 +0000

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import numpy as np
 import streamlit as st
 
 pr = pd.read_csv('pr_2018_anomaly_details.csv')
-pr['key'] = pr['npi'].astype(str) + pr['first_name'] + pr['last_name']
+pr['provider_key'] = pr['npi'].astype(str) + pr['first_name'] + pr['last_name']
 
 # select the state and chache results
 states = np.unique(pr.state)
