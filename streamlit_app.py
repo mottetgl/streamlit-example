96f5234d67d4e792516aa494173de6fc8d047165
mottetgl <31945764+mottetgl@users.noreply.github.com>
2021-11-24 23:33:56 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,11 @@ def filter_state(filename, states):
   df = df.loc[df.state.isin(states), :]
   df['provider_key'] = df['npi'].astype(str) + '  /  ' + df['first_name'] + ' ' + df['last_name'] + '  /  ' + df['specialty']
   df.rename(columns = {'centroid_lat': 'lat', 'centroid_long': 'lon'}, inplace = True)
+  type_dict = {'total_billed': numpy.float64,
+               'total_allowed': numpy.float64,
+               'lat': numpy.float64,
+               'lon': numpy.float64}
+  df = df.astype(type_dict)
   df.index = [""] * len(df)
   return(df)
 pr_phys = filter_state(pr_phys_infile, sel_states)
