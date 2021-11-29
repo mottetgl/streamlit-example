fbc9948771b835f3bc824ccace45233ee956b3ad
mottetgl <31945764+mottetgl@users.noreply.github.com>
2021-11-29 23:01:10 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -103,50 +103,51 @@ st.plotly_chart(fig)
 # show detail for one npi ---------------------------------------------------------------------------------------------------
 # ---------------------------------------------------------------------------------------------------------------------------
 # select an npi
-provider_keys = pr_phys.groupby(['provider_key', 'total_allowed']).size().reset_index().sort_values(by = 'total_allowed', ascending = False)['provider_key'] 
-sel_provider_key = st.selectbox('Select provider', provider_keys)
-@st.cache
-def pull_pr_phys_npi(df, provider_key):
-  return(df[df.provider_key == provider_key])
-pr_phys_one = pull_pr_phys_npi(pr_phys, sel_provider_key)
+with st.expander("Show Anomaly Details (Single Provider)"):
+  provider_keys = pr_phys.groupby(['provider_key', 'total_allowed']).size().reset_index().sort_values(by = 'total_allowed', ascending = False)['provider_key'] 
+  sel_provider_key = st.selectbox('Select provider', provider_keys)
+  @st.cache
+  def pull_pr_phys_npi(df, provider_key):
+    return(df[df.provider_key == provider_key])
+  pr_phys_one = pull_pr_phys_npi(pr_phys, sel_provider_key)
 
-# display aggregate provider info
-st.write(pr_phys_one.loc[:, ['first_name', 'last_name', 'specialty', 'state', 'total_billed', 'total_allowed']].style.format(precision=0))
-
-@st.cache
-def pull_pr_detail_npi(df, provider_key):
-  df = df[df.provider_key == provider_key]
-  df.sort_values(by = 'total_allowed', inplace = True)
-  df.index = [""] * len(df)
-  return(df)
-pr_detail_one = pull_pr_detail_npi(pr_detail, sel_provider_key)
-st.write(pr_detail_one)
-
-# ---------------------------------------------------------------------------------------------------------------------------
-# create anomaly details barchart -------------------------------------------------------------------------------------------
-# ---------------------------------------------------------------------------------------------------------------------------
-fig = px.bar(pr_detail_one,
-             x='total_allowed',
-             y='trunc_hcpcs_key',
-             color='spec_quantile',
-             color_continuous_scale='turbo',
-             custom_data = ['hcpcs_code', 'hcpcs_desc', 'total_billed', 'total_allowed', 'norm_allowed', 'spec_quantile'],
-             range_color=[0, 1],
-             labels={'total_allowed': 'Allowed',
-                     'trunc_hcpcs_key': 'HCPCS',
-                     'spec_quantile': 'Percentile'
-                    },
-             width=800, height=800
-             )
-fig.update_traces(
-    hovertemplate='<br>'.join([
-        'HCPCS Code: %{customdata[0]}',
-        'HCPCS Desc: %{customdata[1]}',
-        '',
-        'Total Billed:  $%{customdata[2]:,.0f}',
-        'Total Allowed: $%{customdata[3]:,.0f} (higher than %{customdata[5]:.1%} of all providers in the same specialty across the US)',
-        'Specialty Average Allowed: $%{customdata[4]:,.0f}',
-    ])
-)
-st.plotly_chart(fig)
+  # display aggregate provider info
+  st.write(pr_phys_one.loc[:, ['first_name', 'last_name', 'specialty', 'state', 'total_billed', 'total_allowed']].style.format(precision=0))
+  
+  @st.cache
+  def pull_pr_detail_npi(df, provider_key):
+    df = df[df.provider_key == provider_key]
+    df.sort_values(by = 'total_allowed', inplace = True)
+    df.index = [""] * len(df)
+    return(df)
+  pr_detail_one = pull_pr_detail_npi(pr_detail, sel_provider_key)
+  st.write(pr_detail_one)
+  
+  # ---------------------------------------------------------------------------------------------------------------------------
+  # create anomaly details barchart -------------------------------------------------------------------------------------------
+  # ---------------------------------------------------------------------------------------------------------------------------
+  fig = px.bar(pr_detail_one,
+               x='total_allowed',
+               y='trunc_hcpcs_key',
+               color='spec_quantile',
+               color_continuous_scale='turbo',
+               custom_data = ['hcpcs_code', 'hcpcs_desc', 'total_billed', 'total_allowed', 'norm_allowed', 'spec_quantile'],
+               range_color=[0, 1],
+               labels={'total_allowed': 'Allowed',
+                       'trunc_hcpcs_key': 'HCPCS',
+                       'spec_quantile': 'Percentile'
+                      },
+               width=800, height=800
+               )
+  fig.update_traces(
+      hovertemplate='<br>'.join([
+          'HCPCS Code: %{customdata[0]}',
+          'HCPCS Desc: %{customdata[1]}',
+          '',
+          'Total Billed:  $%{customdata[2]:,.0f}',
+          'Total Allowed: $%{customdata[3]:,.0f} (higher than %{customdata[5]:.1%} of all providers in the same specialty across the US)',
+          'Specialty Average Allowed: $%{customdata[4]:,.0f}',
+      ])
+  )
+  st.plotly_chart(fig)
 
