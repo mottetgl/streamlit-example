e0de2987257ac38d05b0e448492b10e5a58f318c
mottetgl <31945764+mottetgl@users.noreply.github.com>
2021-11-30 00:42:16 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -123,7 +123,7 @@ with st.expander("Show Anomaly Details (Single Provider)"):
   c3.subheader(pr_phys_one.specialty)
   c4.subheader(pr_phys_one.provider_city + ', ' + pr_phys_one.state)
   c1, c2, c3, c4 = st.columns(4)
-  c1.subheader('${:,.0f}'.format(pr_phys_one.total_allowed))
+  c1.subheader(pr_phys_one.total_allowed)
 
   
   @st.cache
